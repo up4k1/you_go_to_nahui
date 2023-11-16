569bd291fedd6986f34cfb2c24795b51d9f9a381
up4k1 <33572606+up4k1@users.noreply.github.com>
2023-11-16 09:45:54 +0400

    Update yorl.sh

--- yorl.sh
+++ yorl.sh
@@ -1,78 +1,59 @@
 #!/bin/bash
 
-# Цвета для вывода
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[1;33m'
-NC='\033[0m' # No Color
-
-# Функция для проверки статуса выполнения команды
-check_status() {
-  if [ $? -eq 0 ]; then
-    echo -e "${GREEN}Успех.${NC}"
-  else
-    echo -e "${RED}Ошибка. Процесс остановлен.${NC}"
-    exit 1
-  fi
-}
-
-# Проверка и установка Docker
-echo -e "${YELLOW}Проверка и установка Docker...${NC}"
+# Проверка и установка Docker и Docker Compose
 if ! [ -x "$(command -v docker)" ]; then
+  echo 'Установка Docker...'
   sudo apt update
   sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
   sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
   sudo apt update
   sudo apt install -y docker-ce
+  echo 'Docker установлен.'
+else
+  echo 'Docker уже установлен.'
 fi
-check_status
 
-# Проверка и установка Docker Compose
-echo -e "${YELLOW}Проверка и установка Docker Compose...${NC}"
 if ! [ -x "$(command -v docker-compose)" ]; then
-  sudo curl -L "https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  echo 'Установка Docker Compose...'
+  sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
   sudo chmod +x /usr/local/bin/docker-compose
+  echo 'Docker Compose установлен.'
+else
+  echo 'Docker Compose уже установлен.'
 fi
-check_status
 
 # Запрос основной информации от пользователя
-echo -e "${YELLOW}Запрос основной информации...${NC}"
 read -p "Введите имя домена (например, short.ru): " DOMAIN
 read -p "Введите имя пользователя для YOURLS: " YOURLS_USER
 read -p "Введите пароль для YOURLS: " YOURLS_PASS
 read -p "Введите email для Let's Encrypt: " LETSENCRYPT_EMAIL
 read -p "Введите пароль для MySQL: " MYSQL_ROOT_PASSWORD
-check_status
 
-# Создание файла .env для YOURLS
-echo -e "${YELLOW}Создание файла .env для YOURLS...${NC}"
-cat > .env <<EOF
+# Создание директории для YOURLS
+mkdir -p yourls
+
+# Создание .env файла для YOURLS
+cat > yourls/.env <<EOF
 YOURLS_SITE=https://$DOMAIN
 YOURLS_USER=$YOURLS_USER
 YOURLS_PASS=$YOURLS_PASS
 MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD
 MYSQL_DATABASE=yourls
 EOF
-check_status
 
-# Создание файла docker-compose.yml
-echo -e "${YELLOW}Создание файла docker-compose.yml...${NC}"
-cat > docker-compose.yml <<EOF
+# Создание Docker Compose файла (docker-compose.yml)
+cat > yourls/docker-compose.yml <<EOF
 version: '3'
 
 services:
   yourls:
     image: yourls:latest
     restart: always
-    volumes:
-      - ./plugins:/var/www/html/user/plugins
-      - ./plugins:/var/www
-    environment:
-      YOURLS_DB_PASS: "\${MYSQL_ROOT_PASSWORD}"
-      YOURLS_SITE: "\${YOURLS_SITE}"
-      YOURLS_USER: "\${YOURLS_USER}"
-      YOURLS_PASS: "\${YOURLS_PASS}"
+    ports:
+      - 8080:80
+    env_file:
+      - .env
     depends_on:
       - mysql
 
@@ -80,8 +61,8 @@ services:
     image: mysql:5.7
     restart: always
     environment:
-      MYSQL_ROOT_PASSWORD: "\${MYSQL_ROOT_PASSWORD}"
-      MYSQL_DATABASE: "\${MYSQL_DATABASE}"
+      MYSQL_ROOT_PASSWORD: \${MYSQL_ROOT_PASSWORD}
+      MYSQL_DATABASE: \${MYSQL_DATABASE}
 
   nginx:
     image: nginx:latest
@@ -89,29 +70,15 @@ services:
     volumes:
       - ./nginx.conf:/etc/nginx/nginx.conf
       - /etc/letsencrypt:/etc/letsencrypt:ro
-      - ./certbot-data:/var/www/certbot
     ports:
       - 80:80
       - 443:443
     depends_on:
       - yourls
-
-  certbot:
-    image: certbot/certbot
-    volumes:
-      - /etc/letsencrypt:/etc/letsencrypt
-      - /var/lib/letsencrypt:/var/lib/letsencrypt
-      - ./certbot-data:/var/www/certbot
-      - ./certbot.sh:/certbot.sh
-    entrypoint: "/bin/sh -c '/certbot.sh && sleep 2w'"
-    depends_on:
-      - nginx
 EOF
-check_status
 
 # Создание начальной конфигурации Nginx
-echo -e "${YELLOW}Создание начальной конфигурации Nginx...${NC}"
-cat > nginx.conf <<EOF
+cat > yourls/nginx.conf <<EOF
 user  nginx;
 worker_processes  1;
 
@@ -132,11 +99,8 @@ http {
     sendfile        on;
     keepalive_timeout  65;
 
-    include /etc/nginx/conf.d/*.conf;
-
     server {
         listen       80;
-        listen       [::]:80;
         server_name  $DOMAIN;
 
         location ~ /.well-known/acme-challenge/ {
@@ -145,68 +109,33 @@ http {
         }
 
         location / {
-            return 301 https://\$host\$request_uri;
-        }
-    }
-
-    server {
-        listen 443 ssl;
-        server_name $DOMAIN;
-
-        ssl_certificate /etc/letsencrypt/live/$DOMAIN/fullchain.pem;
-        ssl_certificate_key /etc/letsencrypt/live/$DOMAIN/privkey.pem;
-
-        location / {
             proxy_pass http://yourls;
             proxy_set_header Host \$host;
             proxy_set_header X-Real-IP \$remote_addr;
             proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto \$scheme;
         }
     }
 }
 EOF
-check_status
 
 # Создание скрипта для Certbot
-echo -e "${YELLOW}Создание скрипта для Certbot...${NC}"
-cat > certbot.sh <<EOF
+cat > yourls/certbot.sh <<EOF
 #!/bin/sh
 
 if [ ! -e "/etc/letsencrypt/live/$DOMAIN" ]; then
   certbot certonly --webroot --webroot-path /var/www/certbot --email $LETSENCRYPT_EMAIL --agree-tos --no-eff-email -d $DOMAIN --keep-until-expiring --non-interactive
 fi
 
-certbot renew --quiet --no-self-upgrade
+certbot renew
 EOF
-chmod +x certbot.sh
-check_status
+
+chmod +x yourls/certbot.sh
 
 # Создание директории для Certbot
-echo -e "${YELLOW}Создание директории для Certbot...${NC}"
-mkdir -p ./certbot-data
-check_status
+mkdir -p yourls/certbot-data
 
 # Запуск контейнеров
-echo -e "${YELLOW}Запуск контейнеров...${NC}"
+cd yourls
 docker-compose up -d
-check_status
 
-# Проверка корректности работы
-echo -e "${YELLOW}Проверка корректности работы...${NC}"
-if [ -d "/etc/letsencrypt/live/$DOMAIN" ]; then
-  echo -e "${GREEN}Сертификаты SSL успешно получены.${NC}"
-else
-  echo -e "${RED}Ошибка при получении сертификатов SSL.${NC}"
-fi
-
-if [ -d "./certbot-data" ]; then
-  echo -e "${GREEN}Директория Certbot создана.${NC}"
-else
-  echo -e "${RED}Ошибка при создании директории Certbot.${NC}"
-fi
-
-echo -e "${GREEN}Установка завершена. Проверьте настройки вашего DNS и убедитесь, что домен указывает на IP-адрес вашего сервера.${NC}"
-
-
-echo -e "${GREEN}Внимание! Страница доступна по адресу https://${DOMAIN}/admin${NC}"
+echo "Установка завершена. Проверьте настройки вашего DNS и убедитесь, что домен указывает на IP-адрес вашего сервера."
