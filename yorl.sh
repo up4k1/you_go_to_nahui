ebb6810b35399b9686fe781819abd4c1aeb03395
up4k1 <33572606+up4k1@users.noreply.github.com>
2023-11-16 09:54:28 +0400

    Update yorl.sh

--- yorl.sh
+++ yorl.sh
@@ -23,18 +23,19 @@ else
   echo 'Docker Compose уже установлен.'
 fi
 
-# Запрос основной информации от пользователя
-read -p "Введите имя домена (например, short.ru): " DOMAIN
+read -p "Введите имя домена (например, example.com): " DOMAIN
+read -p "Введите ваш email для Let's Encrypt: " EMAIL
 read -p "Введите имя пользователя для YOURLS: " YOURLS_USER
 read -p "Введите пароль для YOURLS: " YOURLS_PASS
-read -p "Введите email для Let's Encrypt: " LETSENCRYPT_EMAIL
 read -p "Введите пароль для MySQL: " MYSQL_ROOT_PASSWORD
 
-# Создание директории для YOURLS
-mkdir -p yourls
+# Создание директории для plugins, если она еще не существует
+mkdir -p yourls-plugins
 
-# Создание .env файла для YOURLS
-cat > yourls/.env <<EOF
+# Запись переменных в .env файл
+cat << EOF > .env
+DOMAIN=$DOMAIN
+EMAIL=$EMAIL
 YOURLS_SITE=https://$DOMAIN
 YOURLS_USER=$YOURLS_USER
 YOURLS_PASS=$YOURLS_PASS
@@ -42,100 +43,56 @@ MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD
 MYSQL_DATABASE=yourls
 EOF
 
-# Создание Docker Compose файла (docker-compose.yml)
-cat > yourls/docker-compose.yml <<EOF
+# Создание файла docker-compose.yml
+cat << EOF > docker-compose.yml
 version: '3'
 
 services:
-  yourls:
-    image: yourls:latest
-    restart: always
+  traefik:
+    image: traefik:v2.5
+    container_name: traefik
     ports:
-      - 8080:80
-    env_file:
-      - .env
+      - "80:80"
+      - "443:443"
+    volumes:
+      - "/var/run/docker.sock:/var/run/docker.sock"
+      - "./letsencrypt:/letsencrypt"
+    command:
+      - "--providers.docker=true"
+      - "--providers.docker.exposedbydefault=false"
+      - "--entrypoints.web.address=:80"
+      - "--entrypoints.websecure.address=:443"
+      - "--certificatesresolvers.myresolver.acme.tlschallenge=true"
+      - "--certificatesresolvers.myresolver.acme.email=\${EMAIL}"
+      - "--certificatesresolvers.myresolver.acme.storage=/letsencrypt/acme.json"
+
+  yourls:
+    image: yourls
+    container_name: yourls
+    labels:
+      - "traefik.enable=true"
+      - "traefik.http.routers.yourls.rule=Host(\`${DOMAIN}\`)"
+      - "traefik.http.routers.yourls.entrypoints=websecure"
+      - "traefik.http.routers.yourls.tls.certresolver=myresolver"
+    environment:
+      YOURLS_DB_HOST: mysql
+      YOURLS_DB_USER: root
+      YOURLS_DB_PASS: \${MYSQL_ROOT_PASSWORD}
+      YOURLS_DB_NAME: \${MYSQL_DATABASE}
+      YOURLS_SITE: \${YOURLS_SITE}
+    volumes:
+      - ./yourls-plugins:/var/www/html/user/plugins
     depends_on:
       - mysql
 
   mysql:
     image: mysql:5.7
-    restart: always
+    container_name: mysql
     environment:
       MYSQL_ROOT_PASSWORD: \${MYSQL_ROOT_PASSWORD}
       MYSQL_DATABASE: \${MYSQL_DATABASE}
-
-  nginx:
-    image: nginx:latest
-    restart: always
     volumes:
-      - ./nginx.conf:/etc/nginx/nginx.conf
-      - /etc/letsencrypt:/etc/letsencrypt:ro
-    ports:
-      - 80:80
-      - 443:443
-    depends_on:
-      - yourls
+      - ./mysql-data:/var/lib/mysql
 EOF
 
-# Создание начальной конфигурации Nginx
-cat > yourls/nginx.conf <<EOF
-user  nginx;
-worker_processes  1;
-
-error_log  /var/log/nginx/error.log warn;
-pid        /var/run/nginx.pid;
-
-events {
-    worker_connections  1024;
-}
-
-http {
-    log_format  main  '\$remote_addr - \$remote_user [\$time_local] "\$request" '
-                      '\$status \$body_bytes_sent "\$http_referer" '
-                      '"\$http_user_agent" "\$http_x_forwarded_for"';
-
-    access_log  /var/log/nginx/access.log  main;
-
-    sendfile        on;
-    keepalive_timeout  65;
-
-    server {
-        listen       80;
-        server_name  $DOMAIN;
-
-        location ~ /.well-known/acme-challenge/ {
-            root /var/www/certbot;
-            allow all;
-        }
-
-        location / {
-            proxy_pass http://yourls;
-            proxy_set_header Host \$host;
-            proxy_set_header X-Real-IP \$remote_addr;
-            proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-        }
-    }
-}
-EOF
-
-# Создание скрипта для Certbot
-cat > yourls/certbot.sh <<EOF
-#!/bin/sh
-
-if [ ! -e "/etc/letsencrypt/live/$DOMAIN" ]; then
-  certbot certonly --webroot --webroot-path /var/www/certbot --email $LETSENCRYPT_EMAIL --agree-tos --no-eff-email -d $DOMAIN --keep-until-expiring --non-interactive
-fi
-
-certbot renew
-EOF
-
-chmod +x yourls/certbot.sh
-
-# Создание директории для Certbot
-mkdir -p yourls/certbot-data
-
-# Запуск контейнеров
-cd yourls
-docker-compose up -d
-
-echo "Установка завершена. Проверьте настройки вашего DNS и убедитесь, что домен указывает на IP-адрес вашего сервера."
+echo "Настройка завершена. Выполните 'docker-compose up -d' для запуска."
